73c1d5fffcfcec53c5397bccb2f849ffe49ffc78
Djurre <djurreaikema@gmail.com>
2024-02-02 22:37:28 +0100

    Exercise 4.2

--- C4E.py
+++ C4E.py
@@ -8,5 +8,18 @@ def recursive_sum(nums):
     return value + recursive_sum(nums)
 
 
-numbers = [2, 4, 6]
-print(recursive_sum(numbers))
+numbers = [1, 1, 1, 1, 1, 1]
+print(recursive_sum(numbers.copy()))
+
+
+# 4.2
+def count_items_in_list(list):
+    list.pop(0)
+
+    if len(list) == 0:
+        return 1
+
+    return 1 + count_items_in_list(list)
+
+
+print(count_items_in_list(numbers.copy()))
